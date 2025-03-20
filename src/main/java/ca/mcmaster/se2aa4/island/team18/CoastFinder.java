5b067e5e421f8457a759d6b5c1f7492c9482730c
Shayaha <jeyasees@mcmaster.ca>
2025-03-20 17:54:50 -0400

    Fix return statement

--- src/main/java/ca/mcmaster/se2aa4/island/team18/CoastFinder.java
+++ src/main/java/ca/mcmaster/se2aa4/island/team18/CoastFinder.java
@@ -1,46 +1,47 @@
 package ca.mcmaster.se2aa4.island.team18;
 
-import java.io.StringReader;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+
 import org.json.JSONArray;
 import org.json.JSONObject;
 import org.json.JSONTokener;
-import java.util.List;
+
+import java.io.StringReader;
 import java.util.ArrayList;
+import java.util.List;
 
 public class CoastFinder {
-    private static final Logger logger = LogManager.getLogger();
+    private static final Logger logger = LogManager.getLogger(CoastFinder.class);
 
     public static List<Integer> getCoastLineKeys(String s) {
+        List<Integer> coastKeys = new ArrayList<>();
         try {
-            //parse the input string into a JSONArray
+            // Parse the input string into a JSONArray
             JSONArray vertices = new JSONArray(new JSONTokener(new StringReader(s)));
-            List<Integer> coastKeys = new ArrayList<>();
 
-            //display coastline vertices
+            // Display coastline vertices
             logger.info("** Coastline Vertices:");
             for (int i = 0; i < vertices.length(); i++) {
                 JSONObject vertex = vertices.getJSONObject(i);
                 int key = vertex.getInt("key");
                 JSONArray vals = vertex.getJSONArray("vals");
 
-                //check the properties of the vertex
-               for (int j = 0; j < vals.length(); j++) {
+                // Check the properties of the vertex
+                for (int j = 0; j < vals.length(); j++) {
                     JSONObject property = vals.getJSONObject(j);
 
-                    //log the vertex key
+                    // Log the vertex key if it is a coastline
                     if (property.getString("p").equals("isCoast") && property.getBoolean("v")) {
                         coastKeys.add(key);
                         break;
                     }
                 }
-
-                return coastKeys;
             }
         } catch (Exception e) {
             logger.error("Error processing the file: ", e);
         }
+
+        return coastKeys;
     }
 }
-
