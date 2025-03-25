bfefe843976436a2657148dd5aeae842cba2aee1
Shayaha <jeyasees@mcmaster.ca>
2025-03-24 20:51:30 -0400

    Fix: BatteryTest runs

--- src/test/java/ca/mcmaster/se2aa4/island/team18/BatteryTest.java
+++ src/test/java/ca/mcmaster/se2aa4/island/team18/BatteryTest.java
@@ -7,28 +7,28 @@ public class BatteryTest {
 
     @Test
     void testInitialLevel() {
-        assertEquals(75, new Battery(75).getPowerLevel());
+        assertEquals(75, new Battery(75).getRemainingPower());
     }
 
     @Test
     void testConsumption() {
         Battery b = new Battery(60);
         b.consume(15);
-        assertEquals(45, b.getPowerLevel());
+        assertEquals(45, b.getRemainingPower());
     }
 
     @Test
     void testDepletion() {
         Battery b = new Battery(20);
         b.consume(20);
-        assertEquals(0, b.getPowerLevel());
+        assertEquals(0, b.getRemainingPower());
     }
 
     @Test
     void testBelowZero() {
         Battery b = new Battery(10);
         b.consume(25);
-        assertEquals(0, b.getPowerLevel());
+        assertEquals(0, b.getRemainingPower());
     }
     
 
